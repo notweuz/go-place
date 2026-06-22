50f87045bcf9f47b8d66da5ac19e88121607cf0e
notweuz <weuz.main@gmail.com>
2026-06-22 19:40:26 +0400

    feat: pixel repository logs

--- internal/domain/pixel/repository.go
+++ internal/domain/pixel/repository.go
@@ -1,8 +1,10 @@
 package pixel
 
 import (
+	"errors"
 	"go-place/internal/model"
 
+	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
@@ -24,33 +26,68 @@ func NewPixelRepository(db *gorm.DB) Repository {
 }
 
 func (r *repository) Create(pixel *model.Pixel) (*model.Pixel, error) {
+	log.Debug().Uint64("x", pixel.X).Uint64("y", pixel.Y).Msg("creating pixel")
 	err := r.db.Create(pixel).Error
+	if err != nil {
+		log.Error().Err(err).Msg("pixel creation failed")
+	}
 	return pixel, err
 }
 
 func (r *repository) Update(pixel *model.Pixel) (*model.Pixel, error) {
+	log.Debug().Uint64("x", pixel.X).Uint64("y", pixel.Y).Msg("updating pixel")
 	err := r.db.Save(pixel).Error
+	if err != nil {
+		log.Error().Err(err).Msg("pixel creation failed")
+	}
 	return pixel, err
 }
 
 func (r *repository) GetByID(id uint64) (*model.Pixel, error) {
+	log.Debug().Uint64("id", id).Msg("getting pixel by id")
 	var entity model.Pixel
 	err := r.db.Where("id = ?", id).First(&entity).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Warn().Uint64("pixel_id", id).Msg("pixel not found")
+		} else {
+			log.Error().Err(err).Uint64("pixel_id", id).Msg("pixel fetch failed")
+		}
+	}
 	return &entity, err
 }
 
 func (r *repository) GetByCoordinates(x, y uint64) (*model.Pixel, error) {
+	log.Debug().Uint64("x", x).Uint64("y", y).Msg("getting pixel by x,y")
 	var entity model.Pixel
 	err := r.db.Where("x = ? AND y = ?", x, y).First(&entity).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Warn().Uint64("x", x).Uint64("y", y).Msg("pixel not found")
+		} else {
+			log.Error().Err(err).Uint64("x", x).Uint64("y", y).Msg("pixel fetch failed")
+		}
+	}
 	return &entity, err
 }
 
 func (r *repository) GetAll() []model.Pixel {
+	log.Debug().Msg("getting all pixels")
 	var entities []model.Pixel
 	r.db.Find(&entities)
+	log.Info().Int("amount", len(entities)).Msg("pixel fetch success")
 	return entities
 }
 
 func (r *repository) Delete(id uint64) error {
-	return r.db.Delete(&model.Pixel{}, id).Error
+	log.Debug().Uint64("id", id).Msg("deleting pixel by id")
+	err := r.db.Delete(&model.Pixel{}, id).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Warn().Uint64("id", id).Msg("can't delete pixel, pixel not found")
+		} else {
+			log.Error().Err(err).Uint64("id", id).Msg("pixel delete failed")
+		}
+	}
+	return err
 }
