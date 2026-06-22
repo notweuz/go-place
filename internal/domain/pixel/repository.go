01d74735b87d458a40111faa1e4257c18ee10802
notweuz <weuz.main@gmail.com>
2026-06-22 19:15:41 +0400

    feat: pixel repository implementation

--- internal/domain/pixel/repository.go
+++ internal/domain/pixel/repository.go
@@ -18,3 +18,39 @@ type Repository interface {
 type repository struct {
 	db *gorm.DB
 }
+
+func NewPixelRepository(db *gorm.DB) Repository {
+	return &repository{db: db}
+}
+
+func (r *repository) Create(pixel *model.Pixel) (*model.Pixel, error) {
+	err := r.db.Create(pixel).Error
+	return pixel, err
+}
+
+func (r *repository) Update(pixel *model.Pixel) (*model.Pixel, error) {
+	err := r.db.Save(pixel).Error
+	return pixel, err
+}
+
+func (r *repository) GetByID(id uint64) (*model.Pixel, error) {
+	var entity model.Pixel
+	err := r.db.Where("id = ?", id).First(&entity).Error
+	return &entity, err
+}
+
+func (r *repository) GetByCoordinates(x, y uint64) (*model.Pixel, error) {
+	var entity model.Pixel
+	err := r.db.Where("x = ? AND y = ?", x, y).First(&entity).Error
+	return &entity, err
+}
+
+func (r *repository) GetAll() []model.Pixel {
+	var entities []model.Pixel
+	r.db.Find(&entities)
+	return entities
+}
+
+func (r *repository) Delete(id uint64) error {
+	return r.db.Delete(&model.Pixel{}, id).Error
+}
