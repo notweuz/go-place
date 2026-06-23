5f1161a47097fec8dcbaa7d36c9024a7a5ee9d75
notweuz <weuz.main@gmail.com>
2026-06-23 21:02:02 +0400

    feat: pixel service

--- internal/domain/pixel/service.go
+++ internal/domain/pixel/service.go
@@ -1,10 +1,12 @@
 package pixel
 
 import (
+	"errors"
 	"go-place/internal/errs"
 	"go-place/internal/model"
 
 	"github.com/gofiber/fiber/v3"
+	"gorm.io/gorm"
 )
 
 type Service interface {
@@ -32,21 +34,45 @@ func (s *service) Create(pixel *model.Pixel) (*model.Pixel, error) {
 }
 
 func (s *service) GetByID(id uint64) (*model.Pixel, error) {
-	//TODO implement me
-	panic("implement me")
+	entity, err := s.repository.GetByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
+		}
+		return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Couldn't get pixel")
+	}
+	return entity, nil
 }
 
 func (s *service) GetByCoordinates(x, y uint64) (*model.Pixel, error) {
-	//TODO implement me
-	panic("implement me")
+	entity, err := s.repository.GetByCoordinates(x, y)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
+		}
+		return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Couldn't get pixel")
+	}
+	return entity, nil
 }
 
 func (s *service) Update(pixel *model.Pixel) (*model.Pixel, error) {
-	//TODO implement me
-	panic("implement me")
+	entity, err := s.repository.Update(pixel)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
+		}
+		return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Couldn't get pixel")
+	}
+	return entity, nil
 }
 
 func (s *service) Delete(id uint64) error {
-	//TODO implement me
-	panic("implement me")
+	err := s.repository.Delete(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
+		}
+		return errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Couldn't delete pixel")
+	}
+	return nil
 }
