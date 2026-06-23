a8c2bf2b0033e4ea1db1a3cf279c4f7f7a09cd20
notweuz <weuz.main@gmail.com>
2026-06-23 21:44:53 +0400

    feat: app graceful shutdown

--- cmd/goplace/main.go
+++ cmd/goplace/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"go-place/internal/config"
 	"go-place/internal/logger"
 	"go-place/internal/server"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -19,5 +24,24 @@ func main() {
 	logger.UpdateLogLevel(cfg.LogLevel)
 
 	app := server.NewApp(cfg)
-	_ = app
+	go func() {
+		log.Info().Msg("Starting fiber application")
+		if err := app.Fiber.Listen(fmt.Sprintf(":%d", app.Config.AppPort)); err != nil {
+			log.Panic().Err(err).Msg("Error starting app")
+		}
+	}()
+
+	log.Debug().Msg("Initialized graceful shutdown")
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err = app.Fiber.ShutdownWithContext(shutdownCtx); err != nil {
+		log.Panic().Err(err).Msg("Error shutting down")
+	} else {
+		log.Info().Msg("Server gracefully stopped")
+	}
 }
