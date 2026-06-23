832c53ad8652777e1e75efe2b4be1dc85f79b51e
notweuz <weuz.main@gmail.com>
2026-06-23 21:05:29 +0400

    refactor: pixel service logging

--- internal/domain/pixel/service.go
+++ internal/domain/pixel/service.go
@@ -6,6 +6,7 @@ import (
 	"go-place/internal/model"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
@@ -26,16 +27,20 @@ func NewService(repository Repository) Service {
 }
 
 func (s *service) Create(pixel *model.Pixel) (*model.Pixel, error) {
+	log.Info().Uint64("pixel_id", pixel.ID).Uint64("x", pixel.X).Uint64("y", pixel.Y).Msg("Attempting to create pixel")
 	entity, err := s.repository.Create(pixel)
 	if err != nil {
+		log.Error().Err(err).Msg("Failed to create pixel")
 		return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Couldn't create pixel")
 	}
 	return entity, nil
 }
 
 func (s *service) GetByID(id uint64) (*model.Pixel, error) {
+	log.Info().Uint64("pixel_id", id).Msg("Attempting to get pixel by id")
 	entity, err := s.repository.GetByID(id)
 	if err != nil {
+		log.Error().Err(err).Uint64("pixel_id", id).Msg("Failed to get pixel by id")
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
 		}
@@ -45,8 +50,10 @@ func (s *service) GetByID(id uint64) (*model.Pixel, error) {
 }
 
 func (s *service) GetByCoordinates(x, y uint64) (*model.Pixel, error) {
+	log.Info().Uint64("x", x).Uint64("y", y).Msg("Attempting to get pixel by coordinates")
 	entity, err := s.repository.GetByCoordinates(x, y)
 	if err != nil {
+		log.Error().Err(err).Uint64("x", x).Uint64("y", y).Msg("Failed to get pixel by coordinates")
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
 		}
@@ -56,8 +63,10 @@ func (s *service) GetByCoordinates(x, y uint64) (*model.Pixel, error) {
 }
 
 func (s *service) Update(pixel *model.Pixel) (*model.Pixel, error) {
+	log.Info().Uint64("pixel_id", pixel.ID).Msg("Attempting to update pixel")
 	entity, err := s.repository.Update(pixel)
 	if err != nil {
+		log.Error().Err(err).Uint64("pixel_id", pixel.ID).Msg("Failed to update pixel")
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
 		}
@@ -67,8 +76,10 @@ func (s *service) Update(pixel *model.Pixel) (*model.Pixel, error) {
 }
 
 func (s *service) Delete(id uint64) error {
+	log.Info().Uint64("pixel_id", id).Msg("Attempting to delete pixel by id")
 	err := s.repository.Delete(id)
 	if err != nil {
+		log.Error().Err(err).Uint64("pixel_id", id).Msg("Failed to delete pixel by id")
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return errs.NewAppError(errs.ErrNotFoundInDB, fiber.StatusNotFound, "Pixel not found")
 		}
