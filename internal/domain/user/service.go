9a8128e6604a8361f45efbb1b6c2110369997df4
notweuz <weuz.main@gmail.com>
2026-06-24 17:45:42 +0400

    refactor: user service functions output arguments

--- internal/domain/user/service.go
+++ internal/domain/user/service.go
@@ -11,10 +11,10 @@ import (
 )
 
 type Service interface {
-	Create(user *model.User) (*model.User, error)
+	Create(user *model.User) error
 	GetByID(id uint64) (*model.User, error)
 	GetAll() []model.User
-	Update(user *model.User) (*model.User, error)
+	Update(user *model.User) error
 	Delete(id uint64) error
 }
 
@@ -22,17 +22,17 @@ type service struct {
 	repository Repository
 }
 
-func (s *service) Create(user *model.User) (*model.User, error) {
+func (s *service) Create(user *model.User) error {
 	log.Info().Uint64("id", user.ID).Str("username", user.Username).Msg("Creating user")
 	err := s.repository.Create(user)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to create user")
 		if errors.Is(err, gorm.ErrDuplicatedKey) {
-			return nil, errs.NewAppError(errs.ErrDuplicatedKey, fiber.StatusConflict, "User with same username already exists")
+			return errs.NewAppError(errs.ErrDuplicatedKey, fiber.StatusConflict, "User with same username already exists")
 		}
-		return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to create user")
+		return errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to create user")
 	}
-	return user, nil
+	return nil
 }
 
 func (s *service) GetByID(id uint64) (*model.User, error) {
@@ -53,21 +53,21 @@ func (s *service) GetAll() []model.User {
 	return s.repository.GetAll()
 }
 
-func (s *service) Update(user *model.User) (*model.User, error) {
+func (s *service) Update(user *model.User) error {
 	log.Info().Uint64("id", user.ID).Str("username", user.Username).Msg("Updating user")
 	err := s.repository.Update(user)
 	if err != nil {
 		log.Error().Err(err).Uint64("id", user.ID).Msg("Failed to update user")
 		switch {
 		case errors.Is(err, gorm.ErrRecordNotFound):
-			return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusNotFound, "User not found")
+			return errs.NewAppError(errs.ErrInternalServerError, fiber.StatusNotFound, "User not found")
 		case errors.Is(err, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to update user")):
-			return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to update user")
+			return errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to update user")
 		default:
-			return nil, errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to update user")
+			return errs.NewAppError(errs.ErrInternalServerError, fiber.StatusInternalServerError, "Failed to update user")
 		}
 	}
-	return user, nil
+	return nil
 }
 
 func (s *service) Delete(id uint64) error {
