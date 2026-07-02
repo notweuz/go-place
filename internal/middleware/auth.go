bfa71c1b3ced0c7a9f8c1390d98ee63b336e31cd
notweuz <weuz.main@gmail.com>
2026-07-02 19:19:52 +0400

    feat: auth middleware get current user id from context

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -7,23 +7,33 @@ import (
 	"strings"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/rs/zerolog/log"
 )
 
 func AuthProtected(cfg *config.Config) fiber.Handler {
 	return func(ctx fiber.Ctx) error {
 		authHeader := ctx.Get("Authorization")
 		if len(authHeader) == 0 || !strings.HasPrefix(authHeader, "Bearer ") {
-			return errs.NewAppError(errs.ErrNotAuthorized, fiber.StatusUnauthorized, "auth token is missing")
+			return errs.UnauthorizedTokenError(errs.ErrNotAuthorized, "token is missing")
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 		userID, err := auth.ParseToken(tokenString, cfg.JwtSecret)
 		if err != nil {
-			return errs.NewAppError(errs.ErrNotAuthorized, fiber.StatusUnauthorized, "invalid token")
+			return errs.UnauthorizedTokenError(err, "token is invalid")
 		}
 
 		ctx.Locals("user_id", userID)
 
 		return ctx.Next()
 	}
+}
+
+func GetCurrentUserID(ctx fiber.Ctx) (uint64, error) {
+	userID, ok := ctx.Locals("user_id").(uint64)
+	if !ok {
+		log.Error().Msg("failed to get user id in context")
+		return 0, errs.UnauthorizedTokenError(errs.ErrNotAuthorized, "failed to get user id in context")
+	}
+	return userID, nil
 }
