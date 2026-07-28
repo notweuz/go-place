cdd272125813a613b9a2f380e290c262f7c6d024
notweuz <weuz.main@gmail.com>
2026-07-28 16:50:03 +0400

    feat: pixel handler with GetByID & GetByCoordinates functions

--- internal/domain/pixel/handler.go
+++ internal/domain/pixel/handler.go
@@ -1,1 +1,55 @@
 package pixel
+
+import (
+	"errors"
+	"go-place/internal/errs"
+	"go-place/internal/model/response"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type Handler interface {
+	GetByID(ctx fiber.Ctx) error
+	GetByCoordinates(ctx fiber.Ctx) error
+}
+
+type handler struct {
+	service Service
+}
+
+func NewHandler(service Service) Handler {
+	return &handler{service: service}
+}
+
+func (h *handler) GetByID(ctx fiber.Ctx) error {
+	id := fiber.Params[uint64](ctx, "id")
+	pixel, err := h.service.GetByID(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, errs.ErrNotFound):
+			return errs.NotFound(err, "pixel with the given ID doesnt exist")
+		}
+		return errs.Internal(err)
+	}
+
+	pixelFull := response.NewPixelFull(pixel.ID, pixel.X, pixel.Y, pixel.UserID, pixel.Color, pixel.CreatedAt, pixel.UpdatedAt)
+
+	return ctx.Status(fiber.StatusOK).JSON(pixelFull)
+}
+
+func (h *handler) GetByCoordinates(ctx fiber.Ctx) error {
+	x := fiber.Params[uint64](ctx, "x")
+	y := fiber.Params[uint64](ctx, "y")
+	pixel, err := h.service.GetByCoordinates(x, y)
+	if err != nil {
+		switch {
+		case errors.Is(err, errs.ErrNotFound):
+			return errs.NotFound(err, "pixel with the given coordinates doesnt exist")
+		}
+		return errs.Internal(err)
+	}
+
+	pixelFull := response.NewPixelFull(pixel.ID, pixel.X, pixel.Y, pixel.UserID, pixel.Color, pixel.CreatedAt, pixel.UpdatedAt)
+
+	return ctx.Status(fiber.StatusOK).JSON(pixelFull)
+}
