96d97a0ce31e78e462bcbdcec274f9e912caad87
notweuz <weuz.main@gmail.com>
2026-07-28 16:50:10 +0400

    feat: pixel response dto

---
+++ internal/model/response/pixel.go
@@ -0,0 +1,26 @@
+package response
+
+import "time"
+
+type PixelFull struct {
+	ID     uint64 `json:"id"`
+	X      uint64 `json:"x"`
+	Y      uint64 `json:"y"`
+	UserID uint64 `json:"author_id"`
+	Color  string `json:"color"`
+
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewPixelFull(id, x, y, userID uint64, color string, createdAt, updatedAt time.Time) *PixelFull {
+	return &PixelFull{
+		ID:        id,
+		X:         x,
+		Y:         y,
+		UserID:    userID,
+		Color:     color,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+	}
+}
