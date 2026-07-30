0c348a33404a790ec8727c11083e2c696df81416
notweuz <weuz.main@gmail.com>
2026-07-30 18:04:29 +0400

    feat: implement task to ensure canvas size

--- internal/server/tasks/canvas.go
+++ internal/server/tasks/canvas.go
@@ -1,7 +1,33 @@
 package tasks
 
-import "go-place/internal/domain/pixel"
+import (
+	"go-place/internal/domain/pixel"
+	"go-place/internal/domain/setting"
+	"go-place/internal/model"
 
-func EnsurePixelsSize(pixelService pixel.Service) {
-	// TODO: task for updating canvas size
+	"github.com/rs/zerolog/log"
+)
+
+func EnsureCanvasSize(pixelService pixel.Service, settingService setting.Service) {
+	s, err := settingService.Get()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get current settings, aborting EnsureCanvasSize task")
+		return
+	}
+	w, h := s.CanvasWidth, s.CanvasHeight
+	for x := uint64(0); x < w; x++ {
+		for y := uint64(0); y < h; y++ {
+			_, err := pixelService.GetByCoordinates(x, y)
+			if err != nil {
+				newPixel := &model.Pixel{
+					X:      x,
+					Y:      y,
+					UserID: 0,
+				}
+				if createErr := pixelService.Create(newPixel); createErr != nil {
+					log.Error().Err(createErr).Uint64("x", x).Uint64("y", y).Msg("Failed to create default pixel during canvas size enforcement")
+				}
+			}
+		}
+	}
 }
