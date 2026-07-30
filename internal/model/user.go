953b58574320ac8acca1db2491523f10d87302d2
notweuz <weuz.main@gmail.com>
2026-07-31 00:26:28 +0400

    feat: add charge regeneration to user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,11 +6,12 @@ type User struct {
 	ID       uint64  `gorm:"primaryKey;autoIncrement"`
 	Username string  `gorm:"not null;uniqueIndex;size:50"`
 	Password string  `gorm:"not null;size:24"`
-	Charges  uint    `gorm:"not null;default:0"`
 	Pixels   []Pixel `gorm:"foreignKey:UserID"`
+	Charges  uint    `gorm:"not null;default:0"`
 
-	CreatedAt time.Time `gorm:"autoCreateTime"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+	LastChargeAt time.Time `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime"`
 }
 
 func NewUser(username, password string) *User {
@@ -18,4 +19,19 @@ func NewUser(username, password string) *User {
 		Username: username,
 		Password: password,
 	}
+}
+
+func (u *User) CurrentCharges(maxCharges uint, regenRate time.Duration) uint {
+	if u.Charges >= maxCharges {
+		return maxCharges
+	}
+	timePassed := time.Since(u.LastChargeAt)
+
+	gainedCharges := uint(timePassed / regenRate)
+	totalCharges := u.Charges + gainedCharges
+	if totalCharges > maxCharges {
+		return maxCharges
+	}
+
+	return totalCharges
 }
