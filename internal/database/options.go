029ffa5725518fe8a66a56d9596cd22e5b708d5d
notweuz <weuz.main@gmail.com>
2026-07-31 15:57:14 +0400

    feat: add database options for locking and transactions

--- internal/database/options.go
+++ internal/database/options.go
@@ -1,11 +1,26 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
 
 type Option func(*gorm.DB) *gorm.DB
 
 func WithPreload(relation string, args ...interface{}) Option {
 	return func(db *gorm.DB) *gorm.DB {
 		return db.Preload(relation, args...)
+	}
+}
+
+func WithLock() Option {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Clauses(clause.Locking{Strength: "UPDATE"})
+	}
+}
+
+func WithTx(tx *gorm.DB) Option {
+	return func(db *gorm.DB) *gorm.DB {
+		return tx
 	}
 }
