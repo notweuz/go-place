f95a83fcd8adfdd4389b4d024af3df8f13905941
notweuz <weuz.main@gmail.com>
2026-07-31 16:04:52 +0400

    feat: remove email validation from login credentials bcs it's username, not email

--- internal/model/request/auth.go
+++ internal/model/request/auth.go
@@ -1,6 +1,6 @@
 package request
 
 type AuthCredentials struct {
-	Login    string `json:"login" validate:"required,email"`
+	Login    string `json:"login" validate:"required"`
 	Password string `json:"password" validate:"required,min=8,max=24"`
 }
