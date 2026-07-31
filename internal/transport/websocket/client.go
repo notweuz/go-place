431f2e525f1d3eecb00b24df0be8a0585e19d5c1
notweuz <weuz.main@gmail.com>
2026-07-31 15:08:20 +0400

    feat: increase websocket client send channel buffer size

--- internal/transport/websocket/client.go
+++ internal/transport/websocket/client.go
@@ -24,7 +24,7 @@ type client struct {
 func NewClient(conn *websocket.Conn) Client {
 	return &client{
 		conn: conn,
-		send: make(chan message.Base, 10),
+		send: make(chan message.Base, 64),
 		done: make(chan struct{}),
 	}
 }
